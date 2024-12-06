1563e53c0b92abde6c0add1cffa1c593470476b7
Mounika934638 <palabandlamounika2@gmail.com>
2024-12-06 14:19:08 +0530

    add

---
+++ Main21.java
@@ -0,0 +1,12 @@
+public class Main21 {
+    public static void main(String[] args) {
+        // Initialize an integer array at the time of declaration
+        int[] array = {10, 20, 30, 40, 50};
+
+        // Print the array values
+        System.out.print("Array values: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
