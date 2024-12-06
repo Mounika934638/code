fc5eb6dc4e0d6f60b063defab31d602f572b117f
Mounika934638 <palabandlamounika2@gmail.com>
2024-12-06 22:27:02 +0530

    add

---
+++ SortArray.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class SortArray {
+    public static void main(String[] args) {
+        // Example array
+        int[] arr = {5, 2, 8, 1, 3};
+
+        // Sorting the array using Arrays.sort() method
+        Arrays.sort(arr);
+
+        // Printing the sorted array
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+}
